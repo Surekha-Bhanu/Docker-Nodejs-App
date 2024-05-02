0b72298bf148cd87933621b3c62663cdee7fd8b0
Surekha-Bhanu <sannapureddy.bhanu@gmail.com>
2024-05-02 03:17:11 +0000

    changes made1

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3005; // Choose your desired port number
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, nodejs!');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
