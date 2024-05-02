9d2ba74b32231d4e359262487427068378484015
Surekha-Bhanu <168653646+Surekha-Bhanu@users.noreply.github.com>
2024-05-02 20:52:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 6000; // Choose your desired port number
+const port = 3007; // Choose your desired port number
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
